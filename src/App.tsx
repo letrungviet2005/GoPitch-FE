f5bca59a43a61b1a4f5b3f1969b8da2a4caea9ce
letrungviet2005 <vitprofb@gmail.com>
2025-07-26 10:57:44 +0700

    feat(ui): fix config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import AppLayout from "./layout/AppLayout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
         <Routes>
           {/* Dashboard Layout */}
           <Route element={<AppLayout />}>
-            <Route index path="/" element={<div>Home Page</div>} />
+            <Route index path="/" element={<Home />} />
             <Route path="/about" element={<div>About Page</div>} />
             <Route path="/contact" element={<div>Contact Page</div>} />
           </Route>
