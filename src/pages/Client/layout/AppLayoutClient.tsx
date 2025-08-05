792b78a604f09c664b295093cd5d76dfe62a927e
letrungviet2005 <vitprofb@gmail.com>
2025-08-05 19:35:45 +0700

    feat(ui): new upload

--- src/pages/Client/layout/AppLayoutClient.tsx
+++ src/pages/Client/layout/AppLayoutClient.tsx
@@ -1,19 +1,19 @@
-// // src/layouts/AppLayout.js
-// import React from "react";
-// import { Outlet } from "react-router-dom";
-// import Header from "./Header/Header";
-// import Footer from "./Footer/Footer";
+// src/layouts/AppLayout.js
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
-// const AppLayoutClient = () => {
-//   return (
-//     <>
-//       <Header />
-//       <main>
-//         <Outlet />
-//       </main>
-//       <Footer />
-//     </>
-//   );
-// };
+const AppLayoutClient = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-// export default AppLayoutClient;
+export default AppLayoutClient;
