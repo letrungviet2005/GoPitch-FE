339bf49e291bc608da8d96ac92616b1233518cf4
letrungviet2005 <vitprofb@gmail.com>
2025-08-07 18:54:13 +0700

    feat(ui): bew upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import SignIn from "./pages/AuthPages/SignIn";
-import NotFound from "./pages/OtherPage/NotFound";
+// import { BrowserRouter as Router, Routes, Route } from "react-router";
+// import SignIn from "./pages/AuthPages/SignIn";
+// import NotFound from "./pages/OtherPage/NotFound";
 
-import AppLayout from "./layout/AppLayout";
-import { ScrollToTop } from "./components/common/ScrollToTop";
+// import AppLayout from "./layout/AppLayout";
+// import { ScrollToTop } from "./components/common/ScrollToTop";
 
-import "../src/authentication/authentication";
-import Home from "./pages/Admin/Dashboard/Home";
-import Pitch from "./pages/Client/Pitch/Pitch";
-import AppLayoutClient from "./pages/Client/layout/AppLayoutClient";
+// import "../src/authentication/authentication";
+// import Home from "./pages/Admin/Dashboard/Home";
+// import Pitch from "./pages/Client/Pitch/Pitch";
+// import AppLayoutClient from "./pages/Client/layout/AppLayoutClient";
 
-import HomePage from "./pages/Client/HomePage/HomePage";
+// import HomePage from "./pages/Client/HomePage/HomePage";
 
-export default function App() {
-  return (
-    <>
-      <Router>
-        <ScrollToTop />
-        <Routes>
-          {/* Dashboard Layout */}
-          <Route element={<AppLayout />}>
-            <Route index path="/admin" element={<Home />} />
+// export default function App() {
+//   return (
+//     <>
+//       <Router>
+//         <ScrollToTop />
+//         <Routes>
+//           {/* Dashboard Layout */}
+//           <Route element={<AppLayout />}>
+//             <Route index path="/admin" element={<Home />} />
 
-            {/* Others Page */}
-          </Route>
+//             {/* Others Page */}
+//           </Route>
 
-          <Route element={<AppLayoutClient />}>
-            <Route index path="/" element={<HomePage />} />
-            <Route path="/pitch" element={<Pitch />} />
+//           <Route element={<AppLayoutClient />}>
+//             <Route index path="/" element={<HomePage />} />
+//             <Route path="/pitch" element={<Pitch />} />
 
-            {/* Others Page */}
-          </Route>
+//             {/* Others Page */}
+//           </Route>
 
-          {/* Auth Layout */}
-          <Route path="/signin" element={<SignIn />} />
+//           {/* Auth Layout */}
+//           <Route path="/signin" element={<SignIn />} />
 
-          {/* Fallback Route */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
-  );
-}
+//           {/* Fallback Route */}
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
