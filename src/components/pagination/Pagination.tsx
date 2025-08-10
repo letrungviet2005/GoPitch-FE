bb3302856347b2151a5e7c27348d857893804145
letrungviet2005 <vitprofb@gmail.com>
2025-08-10 19:57:28 +0700

    feat(ui): new upload

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,42 +1,42 @@
-import classNames from "classnames/bind";
-import styles from "./Pagination.module.scss";
+// import classNames from "classnames/bind";
+// import styles from "./Pagination.module.scss";
 
-const cx = classNames.bind(styles);
+// const cx = classNames.bind(styles);
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  // Tạo mảng số trang
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+// const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+//   // Tạo mảng số trang
+//   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-  return (
-    <div className={cx("pagination")}>
-      {/* Nút Prev */}
-      <button
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        « Prev
-      </button>
+//   return (
+//     <div className={cx("pagination")}>
+//       {/* Nút Prev */}
+//       <button
+//         onClick={() => onPageChange(currentPage - 1)}
+//         disabled={currentPage === 1}
+//       >
+//         « Prev
+//       </button>
 
-      {/* Các số trang */}
-      {pages.map((page) => (
-        <button
-          key={page}
-          onClick={() => onPageChange(page)}
-          className={cx({ active: page === currentPage })}
-        >
-          {page}
-        </button>
-      ))}
+//       {/* Các số trang */}
+//       {pages.map((page) => (
+//         <button
+//           key={page}
+//           onClick={() => onPageChange(page)}
+//           className={cx({ active: page === currentPage })}
+//         >
+//           {page}
+//         </button>
+//       ))}
 
-      {/* Nút Next */}
-      <button
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        Next »
-      </button>
-    </div>
-  );
-};
+//       {/* Nút Next */}
+//       <button
+//         onClick={() => onPageChange(currentPage + 1)}
+//         disabled={currentPage === totalPages}
+//       >
+//         Next »
+//       </button>
+//     </div>
+//   );
+// };
 
-export default Pagination;
+// export default Pagination;
