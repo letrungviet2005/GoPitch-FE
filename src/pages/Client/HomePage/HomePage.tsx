2dddf2dad0f53e37b7233ef716eadab7ce8a119e
letrungviet2005 <vitprofb@gmail.com>
2025-08-11 17:16:56 +0700

    feat(ui): new upload

--- src/pages/Client/HomePage/HomePage.tsx
+++ src/pages/Client/HomePage/HomePage.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import styles from "./HomePage.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 const HomePage = () => {
   return (
-    <div>
-      <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the main content area of the home page.</p>
-      </div>
-      <div>
-        <h2>Featured Content</h2>
-        <p>Here you can find some featured content or announcements.</p>
-      </div>
-      <div>nút đăng nhập nút đăng ký</div>
+    <div className={cx("homepage")}>
+      <header className={cx("hero")}>
+        <h1>Chào mừng đến với GoPitch</h1>
+        <p>Đặt sân thể thao nhanh chóng – Tiện lợi – Chính xác</p>
+        <div className={cx("buttons")}>
+          <button className={cx("btn", "login")}>Đăng nhập</button>
+          <button className={cx("btn", "register")}>Đăng ký</button>
+        </div>
+      </header>
+
+      <section className={cx("featured")}>
+        <h2>Nội dung nổi bật</h2>
+        <p>
+          Tìm kiếm và đặt sân cầu lông, bóng đá, bóng rổ... chỉ với vài bước đơn
+          giản.
+        </p>
+      </section>
+
+      <footer className={cx("footer")}>
+        <p>© 2025 GoPitch. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
--- src/pages/Client/layout/Header/Header.tsx
+++ src/pages/Client/layout/Header/Header.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import style from "./Header.module.scss";
 import classNames from "classnames/bind";
+import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(style);
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={cx("container")}>
-      <div className={cx("logo")}>Logo</div>
-
-      <div className={cx("navigation")}>
-        <a href="#home">Home</a>
-        <a href="#about">Maps</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a>
+      <div className={cx("logo")} onClick={() => navigate("/")}>
+        Logo
       </div>
 
-      <div className={cx("userProfile")}>
+      <div className={cx("navigation")}>
+        <button onClick={() => navigate("/")}>Home</button>
+        <button onClick={() => navigate("/maps")}>Maps</button>
+        <button onClick={() => navigate("/services")}>Services</button>
+        <button onClick={() => navigate("/contact")}>Contact</button>
+      </div>
+
+      <div className={cx("userProfile")} onClick={() => navigate("/profile")}>
         <img
           src="https://tse4.mm.bing.net/th/id/OIP.LkrCBJoljYJlA43RIOjTdwHaHa?pid=Api&P=0&h=180"
           alt="User Avatar"
