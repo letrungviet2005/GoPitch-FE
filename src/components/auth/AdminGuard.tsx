d735747ecefcd0fd8f365d74e6268037f994bf26
letrungviet2005 <vitprofb@gmail.com>
2026-03-02 16:17:53 +0700

    new upload

--- src/components/auth/AdminGuard.tsx
+++ src/components/auth/AdminGuard.tsx
@@ -10,9 +10,8 @@ const AdminGuard = () => {
     return <Navigate to="/signin" replace />;
   }
 
-  // Ở đây ông có thể check thêm Role nếu trong token hoặc logic BE có phân quyền
   // const user = JSON.parse(localStorage.getItem("user") || "{}");
   // if (user.role !== 'ADMIN') return <Navigate to="/403" />;
 
-  return <Outlet />; // Cho phép vào các route con
+  return <Outlet />; 
 };
